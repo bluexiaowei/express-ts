630d2d8ce570e44775a488dde20b7fc38f8a57b8
bluexiaowei <bluexiaowei@aliyun.com>
2021-09-07 16:54:31 +0800

    fix(参数验证不返回错误信息)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import * as logs from "@/log";
 import router from "@/router";
 import bodyParser from "body-parser";
+import { errors } from "celebrate";
 import cookieParser from "cookie-parser";
 import express, { NextFunction, Request, Response } from "express";
-import * as logs from "@/log";
 
 const app = express();
 
@@ -12,11 +13,10 @@ app.use(cookieParser());
 
 app.use(router);
 
+app.use(errors());
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err.name === "UnauthorizedError") {
-    res.status(401).send("invalid token...");
-  } else {
-    next();
+    res.status(401).json({ success: false, message: "invalid token..." });
   }
 });
 
